8fa468658c0d7e763888e0312e7cccbcd0a9ab85
Arne Küderle <a.kuederle@gmail.com>
2018-04-21 18:04:59 +0200

    Implemented conversions to grb

--- rgbpi/controller/color.py
+++ rgbpi/controller/color.py
@@ -4,9 +4,9 @@ import colorsys
 class Color:
     _rgb255 = None
 
-    def __init__(self, rgb255=None, rgb=None, bit24=None, hsv=None):
+    def __init__(self, rgb255=None, rgb=None, grb255=None, grb=None, bit24=None, bit24_grb=None, hsv=None):
         # Check that only 1 value is given
-        if sum(bool(e) for e in (rgb255, rgb, bit24, hsv)) != 1:
+        if sum(bool(e) for e in (rgb255, rgb, bit24, hsv, grb, grb255, bit24_grb)) != 1:
             raise ValueError('Initialize a new Color with only a single color format!')
 
         # Convert to internal rgb255 representation
@@ -18,6 +18,12 @@ class Color:
             self._rgb255 = self.bit24_to_rgb255(bit24)
         elif hsv:
             self._rgb255 = self.hsv_to_rgb255(hsv)
+        elif grb:
+            self._rgb255 = self.grb_to_rgb255(grb)
+        elif grb255:
+            self._rgb255 = self.grb255_to_rgb255(grb255)
+        elif bit24_grb:
+            self._rgb255 = self.bit24_grb_to_rgb255(bit24_grb)
 
     @classmethod
     def rgb255_to_rgb(cls, rgb255):
@@ -43,6 +49,30 @@ class Color:
     def hsv_to_rgb255(cls, hsv):
         return cls.rgb_to_rgb255(colorsys.hsv_to_rgb(*hsv))
 
+    @classmethod
+    def rgb255_to_grb255(cls, rgb255):
+        return rgb255[1], rgb255[0], rgb255[2]
+
+    @classmethod
+    def grb255_to_rgb255(cls, grb255):
+        return cls.rgb255_to_grb255(grb255)
+
+    @classmethod
+    def rgb255_to_grb(cls, rgb255):
+        return cls.rgb255_to_rgb(cls.rgb255_to_grb255(rgb255))
+
+    @classmethod
+    def grb_to_rgb255(cls, grb):
+        return cls.grb255_to_rgb255(cls.rgb_to_rgb255(grb))
+
+    @classmethod
+    def rgb255_to_bit24_grb(cls, rgb255):
+        return cls.rgb255_to_bit24(cls.rgb255_to_grb255(rgb255))
+
+    @classmethod
+    def bit24_grb_to_rgb255(cls, bit24_grb):
+        return cls.rgb255_to_grb255(cls.bit24_to_rgb255(bit24_grb))
+
     @property
     def rgb255(self):
         return self._rgb255
@@ -52,8 +82,20 @@ class Color:
         return self.rgb255_to_rgb(self._rgb255)
 
     @property
+    def grb(self):
+        return self.rgb255_to_grb(self.rgb255)
+
+    @property
+    def grb255(self):
+        return self.rgb255_to_grb255(self.rgb255)
+
+    @property
     def bit24(self):
         return self.rgb255_to_bit24(self._rgb255)
+
+    @property
+    def bit24_grb(self):
+        return self.rgb255_to_bit24_grb(self._rgb255)
 
     @property
     def hsv(self):
--- tests/controller/test_color.py
+++ tests/controller/test_color.py
@@ -22,6 +22,18 @@ class ColorInitTest(unittest.TestCase):
         color = 16711680
         self.assertEqual(self.target_rgb255, Color(bit24=color)._rgb255)
 
+    def test_grb_init(self):
+        color = (0, 1., 0)
+        self.assertEqual(self.target_rgb255, Color(grb=color)._rgb255)
+
+    def test_grb255_init(self):
+        color = (0, 255, 0)
+        self.assertEqual(self.target_rgb255, Color(grb255=color)._rgb255)
+
+    def test_bit24_grb_init(self):
+        color = 65280
+        self.assertEqual(self.target_rgb255, Color(bit24_grb=color)._rgb255)
+
     def test_no_value_init(self):
         with self.assertRaises(ValueError):
             Color()
@@ -40,13 +52,22 @@ class ColorConversionTest(unittest.TestCase):
     def test_rgb255(self):
         self.assertTupleEqual(self.color.rgb255, (255, 0, 0))
 
-    def test_rgb_init(self):
+    def test_rgb(self):
         color = (1., 0, 0)
         self.assertTupleEqual(self.color.rgb, (1, 0, 0))
 
-    def test_hsv_init(self):
+    def test_hsv(self):
         color = (0, 1., 1.)
         self.assertTupleEqual(self.color.hsv, (0, 1., 1.))
 
-    def test_bit24_init(self):
+    def test_bit24(self):
         self.assertEqual(self.color.bit24, 16711680)
+
+    def test_grb(self):
+        self.assertTupleEqual(self.color.grb, (0, 1., 0))
+
+    def test_grb255(self):
+        self.assertTupleEqual(self.color.grb255, (0, 255, 0))
+
+    def test_bit24_grb(self):
+        self.assertEqual(self.color.bit24_grb, 65280)
