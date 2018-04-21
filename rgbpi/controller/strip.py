b408ce3c1ebffb1766cc7f7f073e95552d60bdcf
Arne Küderle <a.kuederle@gmail.com>
2018-04-21 17:22:17 +0200

    Implemented custom color class

--- rgbpi/controller/strip.py
+++ rgbpi/controller/strip.py
@@ -5,17 +5,78 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
+class Color:
+    _rgb255 = None
+
+    def __init__(self, rgb255=None, rgb=None, bit24=None, hsv=None):
+        # Check that only 1 value is given
+        if sum(bool(e) for e in (rgb255, rgb, bit24, hsv)) != 1:
+            raise ValueError('Initialize a new Color with only a single color format!')
+
+        # Convert to internal rgb255 representation
+        if rgb255:
+            self._rgb255 = rgb255
+        elif rgb:
+            self._rgb255 = self.rgb_to_rgb255(rgb)
+        elif bit24:
+            self._rgb255 = self.bit24_to_rgb255(bit24)
+        elif hsv:
+            self._rgb255 = self.hsv_to_rgb255(hsv)
+
+    @classmethod
+    def rgb255_to_rgb(cls, rgb255):
+        return [c/255 for c in rgb255]
+
+    @classmethod
+    def rgb_to_rgb255(cls, rgb):
+        return [int(round(c*255)) for c in rgb]
+
+    @classmethod
+    def rgb255_to_bit24(cls, rgb255):
+        return (rgb255[0] << 16) | (rgb255[1] << 8) | rgb255[2]
+
+    @classmethod
+    def bit24_to_rgb255(cls, bit24):
+        return (bit24 >> 8) & 255, (bit24 >> 16) & 255, bit24 & 255
+
+    @classmethod
+    def rgb255_to_hsv(cls, rgb255):
+        return colorsys.rgb_to_hsv(*cls.rgb255_to_rgb(rgb255))
+
+    @classmethod
+    def hsv_to_rgb255(cls, hsv):
+        return cls.rgb_to_rgb255(colorsys.hsv_to_rgb(*hsv))
+
+    @property
+    def rgb255(self):
+        return self._rgb255
+
+    @property
+    def rgb(self):
+        return self.rgb255_to_rgb(self._rgb255)
+
+    @property
+    def bit24(self):
+        return self.rgb255_to_bit24(self._rgb255)
+
+    @property
+    def hsv(self):
+        return self.rgb255_to_hsv(self._rgb255)
+
+
 ##################################################
 # COLORS
 ##################################################
-red=Color(0,255,0)
-green=Color(255,0,0)
-blue=Color(0,0,255)
-cyan=Color(255,0,255)
-yellow=Color(255,255,0)
-purple=Color(0,255,255)
-white=Color(255,255,255)
-off=Color(0,0,0)
+red = Color(rgb255=(0, 255, 0))
+green = Color(rgb255=(255, 0, 0))
+blue = Color(rgb255=(0, 0, 255))
+cyan = Color(rgb255=(255, 0, 255))
+yellow = Color(rgb255=(255, 255, 0))
+purple = Color(rgb255=(0, 255, 255))
+white = Color(rgb255=(255, 255, 255))
+off = Color(rgb255=(0, 0, 0))
+
 
 ##################################################
 # NEW CLASS
